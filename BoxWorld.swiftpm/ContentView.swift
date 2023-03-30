9fb11b39b0787de4ed59c501d8a5b08d851458f7
Greed <suby0475@gmail,.com>
2023-03-30 19:54:41 +0900

    런치스크린 완료

--- BoxWorld.swiftpm/ContentView.swift
+++ BoxWorld.swiftpm/ContentView.swift
@@ -9,62 +9,63 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-                   // Launch Screen
-                   if isLoading {
-                       launchScreenView.transition(.opacity).zIndex(0)
-                       
-                   }
-                   
-               }
-                .onAppear {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
-//                        3) CHANGED: withAnimation added
-                        withAnimation { isLoading.toggle() }
-                    })
-               }
-        
-        NavigationView{
-        ZStack{
-            
-        
-            BackLine()
-            
-            
-            VStack(alignment: .leading) {
+            // Launch Screen
+            if isLoading {
+                launchScreenView
+                    .transition(.opacity.animation(.easeInOut(duration: 1))).zIndex(1)
+                    .animation(.easeInOut(duration: 2), value: isLoading)
                 
-                TopLayer()
-                
-                VStack(alignment: .leading, spacing: 0){
-                    Title()
-                        .frame(width: 1225.81, height: 50)
-                    
-                    MiniRoom()
-                        .frame(width: 1229, height: 567)
-                }
             }
             
-            PutInMinimi(imagename: "greed")
-                .offset(x: 150, y: 170)
+            NavigationView{
+                ZStack{
+                    
+                    
+                    BackLine()
+                    
+                    
+                    VStack(alignment: .leading) {
+                        
+                        TopLayer()
+                        
+                        VStack(alignment: .leading, spacing: 0){
+                            Title()
+                                .frame(width: 1225.81, height: 50)
+                            
+                            MiniRoom()
+                                .frame(width: 1229, height: 567)
+                        }
+                    }
+                    
+                    PutInMinimi(imagename: "greed")
+                        .offset(x: 150, y: 170)
+                    
+                    PutInMinimi(imagename: "jerry")
+                        .offset(y:40)
+                    
+                    PutInMinimi(imagename: "jinnie")
+                        .offset(x: 350, y: 130)
+                    
+                    PutInMinimi(imagename: "eric")
+                        .offset(x: 500, y: 300)
+                    
+                    PutInMinimi(imagename: "moro")
+                        .offset(x: -180, y: 140)
+                    
+                    PutInMinimi(imagename: "moana")
+                        .offset(x: -430, y: 240)
+                    
+                }//Zstack
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
             
-            PutInMinimi(imagename: "jerry")
-                .offset(y:40)
             
-            PutInMinimi(imagename: "jinnie")
-                .offset(x: 350, y: 130)
-            
-            PutInMinimi(imagename: "eric")
-                .offset(x: 500, y: 300)
-            
-            PutInMinimi(imagename: "moro")
-                .offset(x: -180, y: 140)
-            
-            PutInMinimi(imagename: "moana")
-                .offset(x: -430, y: 240)
-            
-        }//Zstack
         }
-        .navigationViewStyle(StackNavigationViewStyle())
-        
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+                withAnimation {  isLoading.toggle() }
+            })
+        }
         
     }
 }
@@ -78,10 +79,10 @@ struct PutInMinimi: View {
     var body: some View {
         
         Button(action: {print("Button1")}){
-                        Image(imagename)
-                            .resizable()
-                            .frame(width: 105, height: 180)
-                    }
+            Image(imagename)
+                .resizable()
+                .frame(width: 105, height: 180)
+        }
         
     }
 }
